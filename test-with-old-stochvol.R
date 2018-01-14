b6faefe7007f9a989fe1a612444c3ee9c68fc870
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2018-01-14 12:30:07 +0100

    test-with-old-stochvol added

---
+++ test-with-old-stochvol.R
@@ -0,0 +1,27 @@
+# Make sure that the unwanted version of stochvol is not loaded
+
+generate <- FALSE  # generate test file, or do comparison?
+if (generate) {
+  library(stochvol)
+} else {
+  mydevlib <- "~/R/under_development"
+  devtools::install(".", args = paste0("--library=", mydevlib), repos = NULL, dependencies = FALSE)
+  library(RcppArmadillo)
+  library(stochvol, lib.loc = mydevlib)
+}
+
+set.seed(1)
+foo <- svsample(rnorm(1000), 100, 100)
+filename <- "test-with-old-stochvol.RDS"
+
+if (generate) {
+  saveRDS(foo, filename)
+} else {
+  foo.old <- readRDS(filename)
+  foo$runtime <- foo.old$runtime <- NULL
+  if (!identical(foo, foo.old)) {
+    stop("Test failed!")
+  } else {
+    message("Test passed!")
+  }
+}
