e4f5cfb24a15697ae09c1aa051bba48cc26dbd39
Gregor Kastner <gregor.kastner@wu.ac.at>
2019-05-28 01:04:41 +0200

    final bugfixes after introduction of keeptime... for tonight :)

--- R/wrappers.R
+++ R/wrappers.R
@@ -554,7 +554,7 @@ svsample <- function(y, draws = 10000, burnin = 1000, designmatrix = NA,
   res$para <- mcmc(res$para[seq(burnin+thinpara+1, burnin+draws+1, thinpara),,drop=FALSE], burnin+thinpara, burnin+draws, thinpara)
   res$latent <- mcmc(t(res$latent), burnin+thinlatent, burnin+draws, thinlatent)
   if (!exists("arorder")) arorder <- 0L
-  if (keeptime == "all") attr(res$latent, "dimnames") <- list(NULL, paste0('h_', arorder + seq_along(y))) else if (keeptime == "last") attr(res$latent, "dimnames") <- list(NULL, paste0('h_', length(y)))
+  if (keeptime == "all") attr(res$latent, "dimnames") <- list(NULL, paste0('h_', arorder + seq_along(y))) else if (keeptime == "last") attr(res$latent, "dimnames") <- list(NULL, paste0('h_', arorder + length(y)))
   res$latent0 <- mcmc(res$latent0, burnin+thinlatent, burnin+draws, thinlatent)
   if (!any(is.na(designmatrix))) {
     res$beta <- mcmc(res$beta[seq(burnin+thinpara+1, burnin+draws+1, thinpara),,drop=FALSE], burnin+thinpara, burnin+draws, thinpara)
@@ -577,7 +577,7 @@ svsample <- function(y, draws = 10000, burnin = 1000, designmatrix = NA,
 
   if (keeptau) {
     res$tau <- mcmc(t(res$tau), burnin+thinlatent, burnin+draws, thinlatent)
-    if (keeptime == "all") attr(res$tau, "dimnames") <- list(NULL, paste0('tau_', arorder + seq_along(y))) else if (keeptime == "last") attr(res$tau, "dimnames") <- list(NULL, paste0('tau_', length(y)))
+    if (keeptime == "all") attr(res$tau, "dimnames") <- list(NULL, paste0('tau_', arorder + seq_along(y))) else if (keeptime == "last") attr(res$tau, "dimnames") <- list(NULL, paste0('tau_', arorder + length(y)))
   }
 
   res$runtime <- runtime
@@ -1015,12 +1015,12 @@ svlsample <- function (y, draws = 10000, burnin = 1000, designmatrix = NA,
   } else {
     if (any(grep("ar[0-9]+$", as.character(designmatrix)[1]))) {
       arorder <- as.integer(gsub("ar", "", as.character(designmatrix)))
-      if (length(y) <= arorder + 1) stop("Time series 'y' is to short for this AR process.")
+      if (length(y) <= (arorder + 1L)) stop("Time series 'y' is to short for this AR process.")
       designmatrix <- matrix(rep(1, length(y) - arorder), ncol = 1)
       colnames(designmatrix) <- c("const")
       meanmodel <- "constant"
       if (arorder >= 1) {
-        for (i in 1:arorder) {
+        for (i in seq_len(arorder)) {
           oldnames <- colnames(designmatrix)
           designmatrix <- cbind(designmatrix, y[(arorder-i+1):(length(y)-i)])
           colnames(designmatrix) <- c(oldnames, paste0("ar", i))
@@ -1281,7 +1281,7 @@ svlsample <- function (y, draws = 10000, burnin = 1000, designmatrix = NA,
   
   colnames(res$para) <- c("mu", "phi", "sigma", "rho")
   if (!exists("arorder")) arorder <- 0L
-  if (keeptime == "all") colnames(res$latent) <- paste0('h_', arorder + seq_along(y)) else if (keeptime == "last") colnames(res$latent) <- paste0('h_', length(y))
+  if (keeptime == "all") colnames(res$latent) <- paste0('h_', arorder + seq_along(y)) else if (keeptime == "last") colnames(res$latent) <- paste0('h_', arorder + length(y))
   # create svldraws class
   res$runtime <- runtime
   res$y <- y_orig
