95dffd32f79c8154771c7c449ca9469470780c54
Gregor Kastner <gregor.kastner@wu.ac.at>
2019-06-22 15:24:22 +0200

    final minor fix

--- R/util.R
+++ R/util.R
@@ -1,3 +1,14 @@
+#' @describeIn logret Log returns of vectors
+#' @family utilities
+#' @export
+
+logret.default <- function(dat, demean = FALSE, standardize = FALSE, ...) {
+  logretx <- tail(diff(log(dat)), length(dat) - 1)
+  if (all(isTRUE(demean))) logretx <- logretx - mean(logretx)
+  if (all(isTRUE(standardize))) logretx <- logretx / sd(logretx)
+  logretx
+}
+
 myplot <- function (X) {
   par(mfrow = c(4, 1))
   for (i in 1:4) {
