1a0249f40e56fc9c69e58c1264373d37d819e1e7
Gregor Kastner <gregor.kastner@wu.ac.at>
2019-06-24 08:41:56 +0200

    consistent loading message with factorstochvol (very minor update)

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(lib, pkg) {
 	if(interactive() || getOption("verbose")){
-		packageStartupMessage(sprintf("Package %s (%s) attached. To cite, see citation(\"%s\").", pkg, utils::packageDescription(pkg)$Version, pkg))
+		packageStartupMessage(sprintf("Package %s %s attached. To cite, see citation(\"%s\").", pkg, utils::packageDescription(pkg)$Version, pkg))
 	}
 }
