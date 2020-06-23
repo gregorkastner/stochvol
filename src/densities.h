e1e490d67e3aeadc5bb8c1f9076695d7b9475614
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2020-06-23 13:20:40 +0200

    specify namespaces in header densities.h

--- src/densities.h
+++ src/densities.h
@@ -7,16 +7,18 @@
 // (such as acceptance rates or other proportions)
 
 // non-normalized log-density for N(mu, sigma^2)
-inline double logdnorm(
+inline
+double logdnorm(
     double x,
     double mu = 0,
     double sigma = 1) {
- return -log(sigma)-((x-mu)*(x-mu)/(2*sigma*sigma));
+  return -std::log(sigma)-((x-mu)*(x-mu)/(2*sigma*sigma));
 }
 
 // non-normalized log-density for N(mu, sigma^2)
 // with log_sd as input
-inline double logdnorm2(
+inline
+double logdnorm2(
     const double x,
     const double mu,
     const double sigma,
@@ -27,7 +29,8 @@ inline double logdnorm2(
 
 // non-normalized log-density for N(mu, sigma^2)
 // without log(sigma)
-inline double logdnorm3(
+inline
+double logdnorm3(
     const double x,
     const double mu,
     const double sigma) {
@@ -35,7 +38,8 @@ inline double logdnorm3(
 }
 
 // non-normalized density of the gamma distribution
-inline double logdgamma(
+inline
+double logdgamma(
     const double x,
     const double alpha,
     const double beta) {
@@ -43,7 +47,8 @@ inline double logdgamma(
 }
 
 // non-normalized density of the inverse gamma distribution
-inline double logdinvgamma(
+inline
+double logdinvgamma(
     const double x,
     const double alpha,
     const double beta) {
@@ -51,66 +56,73 @@ inline double logdinvgamma(
 }
 
 // non-normalized log-density for Beta(a, b)
-inline double logdbeta(
+inline
+double logdbeta(
     double x,
     double a,
     double b) {
- return (a-1)*log(x)+(b-1)*log(1-x);
+  return (a-1)*std::log(x)+(b-1)*std::log(1-x);
 }
 
 // acceptance ratio for prior matching when sampling sigma
 // Proposal is InvGamma(-0.5, 0)
 // Target is Gamma(.5, 1/(2*Bsigma))
-inline double logacceptrateGamma(
+inline
+double logacceptrateGamma(
     double xnew,
     double xold,
     double Bsigma) {
- return (xold-xnew)/(2*Bsigma);
+  return (xold-xnew)/(2*Bsigma);
 }
 
 // acceptance ratio for log normal random walk
-inline double logacceptrateRW(
+inline
+double logacceptrateRW(
     double xnew,
     double xold,
     double Bsigma,
     int T,
     double z) {
- return .5*(T*log(xold/xnew)+(xold-xnew)/Bsigma+(1/xold-1/xnew)*z);
+  return .5*(T*std::log(xold/xnew)+(xold-xnew)/Bsigma+(1/xold-1/xnew)*z);
 }
 
 // proportion of two beta-distributions with same parameters
 // (evaluated at two different points)
-inline double propBeta(
+inline
+double propBeta(
     double x,
     double y,
     double a,
     double b) {
- return pow(x/y, a-1)*pow((1-x)/(1-y), b-1);
+  return std::pow(x/y, a-1)*std::pow((1-x)/(1-y), b-1);
 }
 
 // full conditional non-normalized posterior log-density of the
 // degrees of freedom parameter nu
-inline double logdnu(
+inline
+double logdnu(
     double nu,
     double sumtau,
     int n) {
- return .5 * nu * (n*log(.5*nu) - sumtau) - n*lgamma(.5*nu);
+  return .5 * nu * (n*std::log(.5*nu) - sumtau) - n*std::lgamma(.5*nu);
 }
 
 // first derivative of logdnu
-inline double dlogdnu(
+inline
+double dlogdnu(
     double nu,
     double sumtau,
     int n) {
- return .5 * (n * ( 1 + log(.5*nu) - Rf_digamma(.5*nu)) - sumtau);
+  return .5 * (n * ( 1 + std::log(.5*nu) - ::Rf_digamma(.5*nu)) - sumtau);
 }
 
 // second derivative of logdnu
-inline double ddlogdnu(
+inline
+double ddlogdnu(
     double nu,
     int n) {
- return .25 * n * (2/nu - Rf_trigamma(.5*nu));
+  return .25 * n * (2/nu - ::Rf_trigamma(.5*nu));
 }
 
-
 #endif
+
